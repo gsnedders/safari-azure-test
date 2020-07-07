006e67103369f8422894a21c98839d35d6be6b33
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2020-07-07 15:35:42 +0300

    Add files via upload

---
+++ safari-technology-preview.rb
@@ -0,0 +1,25 @@
+cask 'safari-technology-preview' do
+  sha256 '4dff98feb0723d0408a313d3b195ac4448a6d9c9dda8d88e6efd03114585d162'
+  
+  url "https://secure-appldnld.apple.com/STP/001-21354-20200624-98a4e372-cb40-4d35-9b8c-8b8abcde5272/SafariTechnologyPreview.dmg"
+  appcast 'https://developer.apple.com/safari/download/'
+  name 'Safari Technology Preview'
+  homepage 'https://developer.apple.com/safari/download/'
+  
+  depends_on macos: '>= :mojave'
+  pkg 'Safari Technology Preview.pkg'
+
+  uninstall delete: '/Applications/Safari Technology Preview.app'
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apple.safaritechnologypreview.sfl*',
+               '~/Library/Caches/com.apple.SafariTechnologyPreview',
+               '~/Library/Preferences/com.apple.SafariTechnologyPreview.plist',
+               '~/Library/SafariTechnologyPreview',
+               '~/Library/Saved Application State/com.apple.SafariTechnologyPreview.savedState',
+               '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview-com.apple.Safari.UserRequests.plist',
+               '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview-com.apple.Safari.WebFeedSubscriptions.plist',
+               '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview.plist',
+               '~/Library/WebKit/com.apple.SafariTechnologyPreview',
+             ]
+end
---
+++ test_webkit.py
@@ -0,0 +1,55 @@
+#coding: utf-8
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import unittest
+import time
+
+class WebKitFeatureStatusTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Safari()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_feature_status_page_search(self):
+        self.driver.get("https://webkit.org/status/")
+
+        # Enter "CSS" into the search box.
+        # Ensures that at least one result appears in search
+        search_box = self.driver.find_element_by_id("search")
+        search_box.send_keys("CSS")
+        value = search_box.get_attribute("value")
+        self.assertTrue(len(value) > 0)
+        search_box.submit()
+        # Count the visible results when filters are applied
+        # so one result shows up in at most one filter
+        feature_count = self.shown_feature_count()
+        print(feature_count)
+        self.assertTrue(feature_count > 0)
+
+    def test_feature_status_page_filters(self):
+        self.driver.get("https://webkit.org/status/")
+
+        wait = WebDriverWait(self.driver, 100)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "filter-toggle")))
+        filters = self.driver.execute_script("return document.querySelectorAll('.filter-toggle')")
+        print("filters found:", *filters, sep = "\n")
+        self.assertTrue(len(filters) > 0)
+
+        # Make sure every filter is turned off.
+        for checked_filter in filter(lambda f: f.is_selected(), filters):
+            checked_filter.click()
+
+    def shown_feature_count(self):
+        wait = WebDriverWait(self.driver, 100)
+        wait.until(lambda driver:len(driver.execute_script("return document.querySelectorAll('li.feature:not(.is-hidden)')"))>0)
+        return len(self.driver.execute_script("return document.querySelectorAll('li.feature:not(.is-hidden)')"))
+
+
+if __name__ == "__main__":
+    unittest.main()
