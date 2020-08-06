803694bd6613ce9b48765bb5101d5bba5136a658
Sam Sneddon <gsnedders@apple.com>
2020-08-06 19:11:01 +0100

    test

--- safari-technology-preview.rb
+++ safari-technology-preview.rb
@@ -1,12 +1,20 @@
 cask 'safari-technology-preview' do
-  sha256 '4dff98feb0723d0408a313d3b195ac4448a6d9c9dda8d88e6efd03114585d162'
-  
-  url "https://secure-appldnld.apple.com/STP/001-21354-20200624-98a4e372-cb40-4d35-9b8c-8b8abcde5272/SafariTechnologyPreview.dmg"
+  if MacOS.version <= :mojave
+    version '108,001-15210-20200610-d35e22e0-d9fa-4503-9988-cf7b2b554e15'
+    sha256 '0299fd2f2836b170a8b633b00d289bcc6913716042e82251af4fa1d5394b87d5'
+  else
+    version '108,001-16091-20200610-09f04256-ae36-4930-b7c4-b1333f8d8e5f'
+    sha256 '22a4d9ca7fb39227cbf4a83be13ad05973171cf88eb6bcc2fac4556a36017357'
+  end
+
+  url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
   appcast 'https://developer.apple.com/safari/download/'
   name 'Safari Technology Preview'
   homepage 'https://developer.apple.com/safari/download/'
-  
+
+  auto_updates true
   depends_on macos: '>= :mojave'
+
   pkg 'Safari Technology Preview.pkg'
 
   uninstall delete: '/Applications/Safari Technology Preview.app'
