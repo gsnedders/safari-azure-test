4764b296a7a292672afc0461e5514ac28ada56ed
Sam Sneddon <gsnedders@apple.com>
2020-08-06 19:08:49 +0100

    test

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -e
+set -x
+
+log stream --info --debug --predicate '(subsystem == "com.apple.TCC")'
 
 cat ~/Library/WebDriver/com.apple.SafariTechnologyPreview.plist
 HOMEBREW_NO_AUTO_UPDATE=1 brew cask install safari-technology-preview.rb
-chmod +x allowremote
-./allowremote
+# chmod +x allowremote
+# ./allowremote
 sudo "/Applications/Safari Technology Preview.app/Contents/MacOS/safaridriver" --enable
 cat ~/Library/WebDriver/com.apple.SafariTechnologyPreview12.plist
