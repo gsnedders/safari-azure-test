b74912412ea0930e1fdea1ae7cf40ddcde31a84b
Sam Sneddon <gsnedders@apple.com>
2023-03-20 21:20:21 +0000

    py3

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ log stream --info --debug --predicate '(subsystem == "com.apple.TCC")' &
 
 ls -lR ~/Library/WebDriver
 
-python -m pip install html5lib packaging requests
-python download.py
+python3 -m pip install html5lib packaging requests
+python3 download.py
 sudo installer -pkg STP.pkg -target LocalSystem
 sudo /Applications/Safari\ Technology\ Preview.app/Contents/MacOS/safaridriver --enable
 
