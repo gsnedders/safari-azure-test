3020c7f12f8cdee1ffef1e10b6ff19c062308704
Sam Sneddon <gsnedders@apple.com>
2023-03-20 21:39:28 +0000

    add more sleep

--- test.sh
+++ test.sh
@@ -10,9 +10,16 @@ ls -lR ~/Library/WebDriver
 python3 -m pip install html5lib packaging requests
 python3 download.py
 sudo installer -pkg STP.pkg -target LocalSystem
+sleep 10
+
 sudo /Applications/Safari\ Technology\ Preview.app/Contents/MacOS/safaridriver --enable
+sleep 10
 
 ls -lR ~/Library/WebDriver
 plutil -p ~/Library/WebDriver/com.apple.SafariTechnologyPreview.plist
+sleep 10
 
 pkill log
+
+sleep 10
+sudo log config --mode "level:default,persist:default"
