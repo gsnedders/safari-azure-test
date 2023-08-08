5fecb197d40e3b44da5a6709a270b20b28261308
Sam Sneddon <gsnedders@apple.com>
2023-08-08 14:11:50 +0100

    try deleting the directory

--- test.sh
+++ test.sh
@@ -14,6 +14,18 @@ sleep 5
 plutil -p ~/Library/WebDriver/com.apple.SafariTechnologyPreview.plist
 sleep 5
 
+rm -rf ~/Library/WebDriver
+sleep 5
+
+ls -lR ~/Library/WebDriver
+sleep 5
+
+plutil -p ~/Library/WebDriver/com.apple.Safari.plist
+sleep 5
+
+plutil -p ~/Library/WebDriver/com.apple.SafariTechnologyPreview.plist
+sleep 5
+
 python3 -m pip install html5lib packaging requests
 python3 download.py
 sudo installer -pkg STP.pkg -target LocalSystem
@@ -22,6 +34,17 @@ sleep 5
 sudo /Applications/Safari\ Technology\ Preview.app/Contents/MacOS/safaridriver --enable
 sleep 5
 
+ls -lR ~/Library/WebDriver
+sleep 5
+
+plutil -p ~/Library/WebDriver/com.apple.Safari.plist
+sleep 5
+
+plutil -p ~/Library/WebDriver/com.apple.SafariTechnologyPreview.plist
+sleep 5
+
+sudo safaridriver --enable
+sleep 5
 
 ls -lR ~/Library/WebDriver
 sleep 5
